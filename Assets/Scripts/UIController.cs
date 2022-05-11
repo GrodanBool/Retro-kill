2ce27582fa0312bfa0cf6863990036534f1355f1
Erik-wiklund <85eriwik@skola.boras.se>
2022-05-11 15:22:06 +0200

    Basic UI layout

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController instance;
+
+    public Slider healthSlider;
+    public Text healthText;
+
+    // public Image damageEffect;
+    // public float damageAlpha = .25f, damageFadeSpeed = 2f;
+
+    public GameObject pauseScreen;
+
+    
+    // public float fadeSpeed = 1.5f;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if(damageEffect.color.a != 0)
+        // {
+        //     damageEffect.color = new Color(damageEffect.color.r, damageEffect.color.g, damageEffect.color.b, Mathf.MoveTowards(damageEffect.color.a, 0f, damageFadeSpeed * Time.deltaTime));
+        // }
+    }
+
+    // public void ShowDamage()
+    // {
+    //     damageEffect.color = new Color(damageEffect.color.r, damageEffect.color.g, damageEffect.color.b, .25f);
+    // }
+}
