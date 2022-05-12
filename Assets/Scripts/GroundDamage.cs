a351d7ea3036c9ddf1f122abd1a308c523e841d7
Erik-wiklund <85eriwik@skola.boras.se>
2022-05-12 14:13:58 +0200

    Fixed color
    Colors on ground now gradually change over time

--- Assets/Scripts/GroundDamage.cs
+++ Assets/Scripts/GroundDamage.cs
@@ -8,23 +8,23 @@ public class GroundDamage : MonoBehaviour
     [SerializeField] bool takeDamage = true;
     private void OnTriggerStay(Collider other)
     {
-        
-        if(other.gameObject.tag == "Player")
-        {     
-             StartCoroutine(TakeDamage(time,other));
+
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(TakeDamage(time, other));
         }
-        
-        
+
+
     }
 
     IEnumerator TakeDamage(float time, Collider other)
     {
         if (takeDamage)
         {
-           other.gameObject.GetComponent<PlayerHealthController>().DamagePlayer(damge);
+            other.gameObject.GetComponent<PlayerHealthController>().DamagePlayer(damge);
             takeDamage = !takeDamage;
             yield return new WaitForSeconds(time);
             takeDamage = !takeDamage;
-        }       
+        }
     }
 }
