0aa55ffc50d9694a0ac1b0b4bbcee6c9512aa47e
Erik-wiklund <85eriwik@skola.boras.se>
2022-05-16 10:24:18 +0200

    Merge branch 'Develop' into Scoring-system
    Maybe fixed?

--- Assets/Scripts/PerkScreen.cs
+++ Assets/Scripts/PerkScreen.cs
@@ -5,7 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class PerkScreen : MonoBehaviour
 {
+    public static PerkScreen instance;
+
     public string goToLevelScreen;
+
+    GameManager manager;
+
+    private string activeMod;
+
+    public string activeMod1;
+
+    public bool lhNotPressed = true;
+    public bool esNotPressed = true;
+    public bool lohNotPressed = true;
+    public bool ngNotPressed = true;
+
+    public string lowHealth;
+    public string noGuns;
+    public string enemySpawn;
+    public string loseHealth;
+
+    private void Awake()
+    {
+        instance = this;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -15,39 +38,103 @@ public class PerkScreen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        switch (activeMod1)
+        {
+            case "lh":
+                if (lhNotPressed)
+                {
+                    lowHealth += "\nLower Health";
+                    activeMod1 = "";
+                    lhNotPressed = !lhNotPressed;
+                }
+                else
+                {
+                    lowHealth = "";
+                    activeMod1 = "";
+                    lhNotPressed = !lhNotPressed;
+                }
+                break;
+            case "ng":
+                if (ngNotPressed)
+                {
+                    noGuns += "\nNo Guns";
+                    activeMod1 = "";
+                    ngNotPressed = !ngNotPressed;
+                }
+                else
+                {
+                    noGuns = "";
+                    activeMod1 = "";
+                    ngNotPressed = !ngNotPressed;
+                }
+                break;
+            case "es":
+                if (esNotPressed)
+                {
+                    enemySpawn += "\nEnemy Spawn Rate";
+                    activeMod1 = "";
+                    esNotPressed = !esNotPressed;
+                }
+                else
+                {
+                    enemySpawn = "";
+                    activeMod1 = "";
+                    esNotPressed = !esNotPressed;
+                }
+                break;
+            case "loh":
+                if (lohNotPressed)
+                {
+                    loseHealth += "\nLose Health";
+                    activeMod1 = "";
+                    lohNotPressed = !lohNotPressed;
+                }
+                else
+                {
+                    loseHealth = "";
+                    activeMod1 = "";
+                    lohNotPressed = !lohNotPressed;
+                }
+                break;
+            default:
+                break;
+        }
 
     }
 
     public void Continue()
     {
+        PlayerPrefs.SetString("activemod", activeMod);
         SceneManager.LoadScene(PlayerPrefs.GetString("CurrentLevel"));
     }
 
     public void LowerHealth()
     {
-        Debug.Log("Lower health chosen");
+        activeMod1 = "lh";
     }
 
     public void NoGuns()
     {
-        Debug.Log("No guns");
+        activeMod1 = "ng";
 
     }
 
     public void EnemySpawnRate()
     {
-        Debug.Log("Enemies spawn rate lowered");
+        activeMod1 = "es";
 
     }
 
-    public void LooseHealth()
+    public void LoseHealth()
     {
-        Debug.Log("Loosing health continously");
+        activeMod1 = "loh";
 
     }
 
     public void StartGame()
     {
+        activeMod = lowHealth + noGuns + enemySpawn + loseHealth;
+        PlayerPrefs.SetString("activemod", activeMod);
         SceneManager.LoadScene(goToLevelScreen);
 
         PlayerPrefs.SetString("CurrentLevel", "");
