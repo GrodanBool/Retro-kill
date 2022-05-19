3a53474929de0aa9b86cb62b7c4a77650236178e
Grodan Bool <nils.franzen99@gmail.com>
2022-05-19 14:22:38 +0200

    database:)

--- Assets/Scripts/HighScoreScripts/ScoreManager.cs
+++ Assets/Scripts/HighScoreScripts/ScoreManager.cs
@@ -1,25 +1,66 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 public class ScoreManager : MonoBehaviour
 {
     private ScoreData sd;
+    MongoClient client = new MongoClient("mongodb+srv://Retro-kill:IAmRetroKill@retro-kill-db.23cj7.mongodb.net/?retryWrites=true&w=majority");
+    IMongoDatabase database;
+    IMongoCollection<BsonDocument> collection;
     void Awake()
     {
         var json = PlayerPrefs.GetString("scores", "{}");
         sd = JsonUtility.FromJson<ScoreData>(json);
     }
 
+    void Start()
+    {
+        database = client.GetDatabase("score");
+        collection = database.GetCollection<BsonDocument>("playerscore");
+    }
+
     public IEnumerable<Score> GetHighScores()
     {
         IEnumerable<Score> scores = new List<Score>();
         IEnumerable<Score> scoresTop5 = new List<Score>();
-
         scores = sd.scores.OrderByDescending(x => x.score);
         scoresTop5 = scores.Take(5).ToList();
-
         return scoresTop5;
+    }
+
+    public async void SaveScore(string name, float score)
+    {
+        BsonDocument document = new BsonDocument { { name, score} };
+        await collection.InsertOneAsync(document);
+    }
+
+    public async Task<List<Score>> GetScore()
+    {
+        var allScoresTask = collection.FindAsync(new BsonDocument());
+        var scoresAwaited = await allScoresTask; 
+        
+        List<Score> scoresList = new List<Score>();
+        foreach (var score in scoresAwaited.ToList())
+        {
+            scoresList.Add(Deserialize(score.ToString()));
+        }
+
+        return scoresList;
+    }
+
+    private Score Deserialize(string rawJson)
+    {
+        var stringNoObjectId = rawJson.Substring(rawJson.IndexOf("),") + 2);
+        string testString = "{ " + stringNoObjectId;
+
+        var test = JsonConvert.DeserializeObject(testString).ToString();
+        var score = JsonUtility.FromJson<Score>(test);
+        return score;
     }
 
     public void AddScore(Score score)
@@ -42,7 +83,6 @@ public class ScoreManager : MonoBehaviour
     public void SaveScore()
     {
         var json = JsonUtility.ToJson(sd);
-        Debug.Log(json);
         PlayerPrefs.SetString("scores", json);
     }
 }
--- Assets/Scripts/HighScoreScripts/ScoreUi.cs
+++ Assets/Scripts/HighScoreScripts/ScoreUi.cs
@@ -1,22 +1,18 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ScoreUi : MonoBehaviour
 {
     public RowUi rowUi;
     public ScoreManager scoreManager;
-
     void Start()
     {
-        //add fake data
-        scoreManager.AddScore(new Score(id: 1, name: "Viktor", score: 69420));
-        scoreManager.AddScore(new Score(id: 2, name: "Erik", score: 420));
-        scoreManager.AddScore(new Score(id: 3, name: "Nils", score: 69));
-        scoreManager.AddScore(new Score(id: 4, name: "Vem?", score: 12));
-        scoreManager.AddScore(new Score(id: 5, name: "Vad?", score: 54));
-        scoreManager.AddScore(new Score(id: 6, name: "Hur?", score: 190000));
+        GetList();
+
         //gets highscores
         var scores = scoreManager.GetHighScores().ToArray();
+
         //shows 5 highest scores
         for (int i = 0; i < scores.Length; i++)
         {
@@ -24,6 +20,15 @@ public class ScoreUi : MonoBehaviour
             row.rank.text = (i + 1).ToString();
             row.playerName.text = scores[i].name;
             row.score.text = scores[i].score.ToString();
+        }
+    }
+
+    private async void GetList()
+    {
+        List<Score> scoreList = await scoreManager.GetScore();
+        foreach (Score score in scoreList)
+        {
+            scoreManager.AddScore(score);
         }
     }
 }
