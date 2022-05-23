a58618895a73f341c874a16fe78ca843c15934bc
Grodan Bool <nils.franzen99@gmail.com>
2022-05-23 15:20:06 +0200

    Story added for singleplayer

--- Assets/Scripts/LevelSelectScreen.cs
+++ Assets/Scripts/LevelSelectScreen.cs
@@ -1,11 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelSelectScreen : MonoBehaviour
 {
-    public string lvl1,lvl2, returnTo;
+    public string lvl1, lvl2, returnTo;
+    public Image story;
+    public Text storyText;
 
     // Start is called before the first frame update
     void Start()
@@ -16,13 +17,19 @@ public class LevelSelectScreen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (story.gameObject.activeInHierarchy)
+        {
+            story.color = new Color(story.color.r, story.color.g, story.color.b, Mathf.MoveTowards(story.color.a, 1f, 1 * Time.deltaTime));
+            if (story.color.a >= 1)
+            {
+                storyText.color = new Color(storyText.color.r, storyText.color.g, storyText.color.b, Mathf.MoveTowards(storyText.color.a, 100f, 1 * Time.deltaTime));
+            }
+        }
     }
     public void Level1()
     {
-        AudioManager.instance.StopBGM();
-        SceneManager.LoadScene(lvl1);
-        PlayerPrefs.SetString("CurrentLevel", "");
+        story.gameObject.SetActive(true);
+        Invoke("LoadLevel1", 10);
     }
     public void Level2()
     {
@@ -33,6 +40,13 @@ public class LevelSelectScreen : MonoBehaviour
     public void ReturnTo()
     {
         SceneManager.LoadScene(returnTo);
+        PlayerPrefs.SetString("CurrentLevel", "");
+    }
+
+    public void LoadLevel1()
+    {
+        AudioManager.instance.StopBGM();
+        SceneManager.LoadScene(lvl1);
         PlayerPrefs.SetString("CurrentLevel", "");
     }
 }
