41627d1e78f5efdfa88ce72b2508056aaf59717f
Grodan Bool <nils.franzen99@gmail.com>
2022-05-31 13:26:58 +0200

    Squashed commit of the following:
    commit a6ed6849448843aed603a7167710e2e0f31acf1f
    Author: Grodan Bool <nils.franzen99@gmail.com>
    Date:   Tue May 31 09:29:53 2022 +0200
    
        sound level fix
    
    commit 55380f58198aa8287648a93e7bcec508d83d7350
    Author: Grodan Bool <nils.franzen99@gmail.com>
    Date:   Tue May 31 09:22:38 2022 +0200
    
        ramp removal
    
    commit 53a54e77456929d03be58a8d914652d16046d6cc
    Author: Grodan Bool <nils.franzen99@gmail.com>
    Date:   Tue May 31 09:14:58 2022 +0200
    
        Enemy manager moved
    
    commit c43afac7dda261f481442ff6afefc52b380bb839
    Author: Grodan Bool <nils.franzen99@gmail.com>
    Date:   Tue May 31 09:11:20 2022 +0200
    
        Level1 modifcations
    
    commit 002bfee55d6f9f25a1cb4986c77887d87ad17bf1
    Author: Grodan Bool <nils.franzen99@gmail.com>
    Date:   Tue May 31 09:05:28 2022 +0200
    
        smol change
    
    commit 919db6fe822eeeb91d1bfd89b877365b1fc7811d
    Author: Grodan Bool <nils.franzen99@gmail.com>
    Date:   Tue May 31 09:03:53 2022 +0200
    
        added backdrop to level 1

---
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,144 @@
+using Mirror;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class EnemyManager : MonoBehaviour
+{
+    public static EnemyManager instance;
+
+    public GameObject enemyPrefab;
+    public GameObject onlineEnemyPrefab;
+
+    public Transform[] spawnPoints;
+
+    [Header("Spawn after x seconds")]
+    public int spawnRate;
+
+    private float spawnCounter;
+
+    private int nrOfSpawnPoints;
+
+    private bool spawnComplete = false;
+    private bool useSpawnPoints = false;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nrOfSpawnPoints = spawnPoints.Length;
+
+        if (SceneManager.GetActiveScene().name != "OnlineLevel")
+        {
+            useSpawnPoints = true;
+            SpawnNewEnemy();
+        }
+        else if (SceneManager.GetActiveScene().name == "OnlineLevel")
+        {
+            useSpawnPoints = true;
+            SpawnNewOnlineEnemy();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnCounter > 0)
+        {
+            spawnCounter -= Time.deltaTime;
+        }
+        else if (spawnCounter <= 0 && SceneManager.GetActiveScene().name != "OnlineLevel")
+        {
+            SpawnNewEnemy();
+            spawnCounter = spawnRate;
+        }
+        else if (spawnCounter <= 0 && SceneManager.GetActiveScene().name == "OnlineLevel")
+        {
+            SpawnNewOnlineEnemy();
+            spawnCounter = spawnRate;
+        }
+    }
+
+    public void SpawnNewEnemyFromSpawnPoint()
+    {
+        Instantiate(enemyPrefab, spawnPoints[Random.Range(0, nrOfSpawnPoints)].transform.position, Quaternion.identity);
+    }
+
+    [Server]
+    public void SpawnNewOnlineEnemy()
+    {
+        SpawnOnlineNewEnemyFromSpawnPoint();
+    }
+
+    [Server]
+    public void SpawnOnlineNewEnemyFromSpawnPoint()
+    {
+        NetworkServer.Spawn(Instantiate(onlineEnemyPrefab, spawnPoints[Random.Range(0, nrOfSpawnPoints)].transform.position, Quaternion.identity));
+    }
+
+    public async void SpawnNewEnemy()
+    {
+        if (!useSpawnPoints)
+        {
+            if (!spawnComplete)
+            {
+                await InstantiateNewEnemy();
+            }
+        }
+        else
+        {
+            SpawnNewEnemyFromSpawnPoint();
+        }
+    }
+
+    public Task<bool> InstantiateNewEnemy()
+    {
+        Instantiate(enemyPrefab, RandomNavmeshSpawnLocation(PlayerController.instance.transform.position, 25), Quaternion.identity);
+        return Task.FromResult(true);
+    }
+
+    public Vector3 RandomNavmeshSpawnLocation(Vector3 playerPostition, int maxRadius)
+    {
+        List<ColliderPositions> colliderPositions = new List<ColliderPositions>();
+        Vector3 finalPosition = Vector3.zero;
+        bool positionFound = false;
+        NavMeshHit hit;
+
+        var test = Physics.OverlapSphere(playerPostition, maxRadius);
+
+        Physics.OverlapSphere(playerPostition, maxRadius).ToList()
+                                                         .ForEach(a => colliderPositions
+                                                         .Add(new ColliderPositions() { Position = a, Tried = false }));
+
+
+        while (!positionFound && colliderPositions.Any(a => a.Tried == false))
+        {
+            Vector3 randomNavmeshSpawn = colliderPositions.Where(a => a.Tried == false).ToList()
+                                         [Random.Range(0, colliderPositions.Count)].Position.transform.position;
+
+            colliderPositions.Where(a => a.Position.transform.position == randomNavmeshSpawn)
+                             .Select(a => { a.Tried = true; return a; })
+                             .ToList();
+
+            if (NavMesh.SamplePosition(randomNavmeshSpawn, out hit, maxRadius, 1))
+            {
+                finalPosition = hit.position;
+                positionFound = true;
+            }
+        }
+        return finalPosition;
+    }
+}
+
+public class ColliderPositions
+{
+    public Collider Position;
+    public bool Tried = false;
+}
